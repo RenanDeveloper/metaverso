8f991026640084135f8d352eda02db48c0ccec59
RenanDeveloper <renantameirao@gmail.com>
2023-01-15 16:56:33 -0300

    destaque temporário para lista de empresas

---
+++ main.js
@@ -0,0 +1,7 @@
+function destacar(){
+    var empresas = document.getElementById('empresas');
+
+    empresas.style = 'border-radius: 15px'
+    empresas.style.animation = 'animation-destacar 2s ease-out'
+}
+
